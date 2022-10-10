b618325dfb498eea101d437b6fddc825e9b8f7ec
Ritulja <104376569+Ritulja@users.noreply.github.com>
2022-10-10 20:21:43 +0100

    Added the layout

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a
-            href="https://taupe-bienenstitch-309e9f.netlify.app/"
-            target="_blank"
-            without
-            rel="noreferrer"
-          >
-            Margareta Krajnikovics
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/Ritulja/react-weather-app"
-            target="_blank"
-            without
-            rel="noreferrer"
-          >
-            open-sourced{" "}
-          </a>{" "}
-          on GitHub.
-        </p>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://taupe-bienenstitch-309e9f.netlify.app/"
+              target="_blank"
+              without
+              rel="noreferrer"
+            >
+              Margareta Krajnikovics
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/Ritulja/react-weather-app"
+              target="_blank"
+              without
+              rel="noreferrer"
+            >
+              open-sourced{" "}
+            </a>{" "}
+            on GitHub.
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,48 @@
 import React from "react";
+import "./Weather.css";
 
-export default function Weather () {
-    return (
-
-    )
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>London</h1>
+      <ul>
+        <li>Monday 19:17</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            class="wob_tci"
+            alt="Clear"
+            src="//ssl.gstatic.com/onebox/weather/64/sunny.png"
+            id="wob_tci"
+            data-atf="1"
+            data-frt="0"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 2%</li>
+            <li>Humiduty: 58%</li>
+            <li>Wind: 3mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
