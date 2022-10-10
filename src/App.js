9c35683b6e9db54967a6f6161b45ad8cfc2e108b
Ritulja <104376569+Ritulja@users.noreply.github.com>
2022-10-10 18:25:23 +0100

    Added open-sourced URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://taupe-bienenstitch-309e9f.netlify.app/"
+            target="_blank"
+            without
+            rel="noreferrer"
+          >
+            Margareta Krajnikovics
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Ritulja/react-weather-app"
+            target="_blank"
+            without
+            rel="noreferrer"
+          >
+            open-sourced{" "}
+          </a>{" "}
+          on GitHub.
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
